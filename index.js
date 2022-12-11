91d739627cb6a42c77938dd7fe08424f90674e6d
João <joaobrit0000000000@gmail.com>
2022-12-11 21:49:36 +0000

    Fix piece movement. Add more pieces

--- index.js
+++ index.js
@@ -62,6 +62,7 @@ Game.prototype.eventListener=function(){
             let clicked_square=item_class[1]+item_class[2];
             
             if(hasPiece(clicked_square)==true){
+
                 if(game.current_piece==undefined){
                     game.current_piece=clicked_square;  
                 }else{
@@ -75,6 +76,14 @@ Game.prototype.eventListener=function(){
                 console.log("piece here");
                 
             }else{
+
+                //if piece is already selected and current click is on empty space
+                if(game.current_piece!=undefined){
+                    console.log("moving to this place!"); 
+                    
+                    bin(item_class,game.current_piece);
+                    game.current_piece=undefined;
+                }
                 console.log("no piece here");
             }
             
@@ -82,19 +91,31 @@ Game.prototype.eventListener=function(){
     });
 }
 //to make criss cross
-function bin(){
-    const pawn_div=document.querySelector(`.p${pawn.position}`);
+function bin(item_class,selected_piece_class){
+    const pawn_div=document.querySelector(`.p${selected_piece_class}`);
     pawn_div.innerHTML="";
-    pawn.position=item_class[1]+item_class[2];
-    pawn.updatePosition(); 
+    game.pawns[pawn_finder(selected_piece_class)].position=item_class[1]+item_class[2];
+    game.pawns[pawn_finder(item_class[1]+item_class[2])].updatePosition();
 }
 
 //experiments unorganized code 
+
+function pawn_finder(item_class){
+    let item_index=0;
+    game.pawns.forEach((item,index)=>{
+        if(item.position==item_class){
+            item_index=index;
+        }
+    });
+    return item_index;
+}
 
 //useless function
 function hasPiece(square){
     let condition=false;
     
+
+    //change this to accept all of the pieces
     game.pawns.forEach((item)=>{
         if(item.position==square){
             
@@ -113,22 +134,48 @@ game.eventListener();
 function Pawn(position){
     this.position=position;
     this.canMove=false;
+    this.color="black";
 }
 
 Pawn.prototype.updatePosition=function (){
     
-    this.pawn=document.querySelector(`.p${this.position}`);
+    this.pawn=document.querySelector(`${game.board[this.position-1]}`);
 
     //square10.style="background-color:red";
     this.img=document.createElement("img");
     this.img.className="pawn";
-    this.img.src="pawn.png";
+    this.img.src=`pawn_${this.color}.png`;
     this.pawn.appendChild(this.img);
 }
 
 //basic pawn testing setup
-game.pawns.push(new Pawn(20));
-game.pawns.push(new Pawn(22));
-game.pawns[0].updatePosition();
-game.pawns[1].updatePosition();
+
+
+function pawns_creator(){
+    let counter=0;
+    //black pawns
+    for(var i=9;i<=16;i++){
+        game.pawns.push(new Pawn(i));
+        console.log(game.pawns[i-9]);
+        console.log(game.pawns)
+        game.pawns[i-9].updatePosition();
+        counter++;
+    }
+    game.pawns.forEach((pawn)=>{
+        pawn.updatePosition(pawn.color);
+    });
+
+    //white pawns
+    for(var i=49;i<=56;i++){
+        game.pawns.push(new Pawn(i));
+        console.log(game.pawns[counter]);
+        console.log(game.pawns)
+        game.pawns[counter].color="white";
+        game.pawns[counter].updatePosition();
+        counter++;
+    }
+    
+
+}
+pawns_creator();
 console.log(game.board);
